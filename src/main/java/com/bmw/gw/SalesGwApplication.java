b640188317fb1e0b3c7c84ff919da36981b95fc5
yuanbing1113 <yuanbing1113@gmail.com>
2019-08-05 22:48:38 +0800

    first commit

---
+++ src/main/java/com/bmw/gw/SalesGwApplication.java
@@ -0,0 +1,17 @@
+package com.bmw.gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication()
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class SalesGwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesGwApplication.class, args);
+	}
+
+}
